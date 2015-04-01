89cdb009d2a3674577069a453ad309bca649e83e
maetl <me@maetl.net>
2015-04-02 01:02:59 +1100

    Tidy up the integration spec using contexts and separate scopes

--- spec/integration/adapter_spec.rb
+++ spec/integration/adapter_spec.rb
@@ -1,71 +1,83 @@
 require 'spec_helper'
-
 require 'rom/lint/spec'
 
 describe 'Neo4j adapter' do
-
-  # TODO: clean up the global state and allow multiple contexts to run cleanly
-
-  subject(:rom) { setup.finalize }
-
   let(:dsn) { 'http://localhost:7474' }
   let(:setup) { ROM.setup(:neo4j, dsn) }
 
-  before do
+  context 'with movies relation and mapper' do
+    let(:rom) do
+      setup.relation(:movies) do
+        matches m: :Movie
+        #returns m: [:title, :released, :tagline]
+        returns ['m.title AS title', 'm.released AS released', 'm.tagline as tagline']
 
-    setup.relation(:movies) do
-      matches m: :Movie
-      returns m: [:title, :released, :tagline]
-
-      def titled(title)
-        where('m.title' => title)
+        def by_title(title)
+          where('m.title' => title)
+        end
       end
 
+      setup.mappers do
+        define(:movies) do
+          relation :movies
+          model name: 'Movie'
+          #prefix :m
+          #prefix_separator '.'
+          attribute :title
+          attribute :released
+          attribute :tagline
+        end
+      end
+
+      setup.finalize
     end
 
-    setup.relation(:directors) do
-      matches '(director:Person)-[:DIRECTED]->(movie:Movie)'
-      returns 'DISTINCT director.name as name'
-
-      def by_movie(title)
-        where('movie.title' => title)
-      end
-
+    let(:movies) do
+      rom.relation(:movies)
     end
 
-    setup.mappers do
-
-      define(:movies) do
-        model name: 'Movie'
-
-        prefix :m
-        prefix_separator '.'
-
-        attribute :title
-        attribute :released
-        attribute :tagline
+    it 'maps movies #by_title' do
+      movies.by_title('The Matrix').as(:movies).first.tap do |movie|
+        expect(movie).to be_a(Movie)
+        expect(movie.title).to eql('The Matrix')
+        expect(movie.released).to eql(1999)
+        expect(movie.tagline).to eql('Welcome to the Real World')
       end
-
-      define(:directors) do
-        model name: 'Director'
-
-        attribute :name
-      end
-
     end
+
   end
 
-  describe 'env#read' do
-    it 'returns mapped object from #titled relation' do
-      movie = rom.read(:movies).titled('The Matrix').first
+  context 'with directors relation and mapper' do
+    let(:rom) do
+      setup.relation(:directors) do
+        matches '(director:Person)-[:DIRECTED]->(movie:Movie)'
+        returns 'DISTINCT director.name AS name'
 
-      expect(movie.title).to eql('The Matrix')
-      expect(movie.released).to eql(1999)
-      expect(movie.tagline).to eql('Welcome to the Real World')
+        def by_movie(title)
+          where('movie.title' => title)
+        end
+      end
 
-      director = rom.read(:directors).by_movie('RescueDawn').first
+      setup.mappers do
+        define(:directors) do
+          relation :directors
+          model name: 'Director'
+          attribute :name
+        end
+      end
 
-      expect(director.name).to eql('Werner Herzog')
+      setup.finalize
+    end
+
+    let(:directors) do
+      rom.relation(:directors)
+    end
+
+    it 'maps directors #by_movie_title' do
+      directors.by_movie('RescueDawn').as(:directors).first.tap do |director|
+        expect(director).to be_a(Director)
+        expect(director.name).to eql('Werner Herzog')
+      end
     end
   end
 end
