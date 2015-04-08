28ad68e44cb955a96ac8f850041f8eca5ad34b49
maetl <me@maetl.net>
2015-04-09 00:13:41 +1000

    Return raw hash results from Neo4j::Core::Query

--- lib/rom/neo4j/support/core_ext.rb
+++ lib/rom/neo4j/support/core_ext.rb
@@ -1,44 +1,19 @@
 # Extensions to the Neo4j::Core library making it easier to integrate with ROM.
 module Neo4j
   module Core
+    UnsupportedDriverError = Class.new(Error)
     # DSL for generating Cypher queries and enumerating their results
     #
     # @see http://www.rubydoc.info/gems/neo4j-core/Neo4j/Core/Query
     class Query
-      # def each
-      #   response = self.response
-      #   if response.is_a?(Neo4j::Server::CypherResponse)
-      #     response.to_node_enumeration
-      #   else
-      #     Neo4j::Embedded::ResultWrapper.new(response, to_cypher)
-      #   end.each { |object| yield object }
-      # end
-    end
-  end
-  module Server
-    class CypherResponse
-      # Coerces result objects from structs to hashes in results. 
-      class HashEnumeration
-        def each(&block)
-          @response.each_data_row do |row|
-            yield(row.each_with_index.each_with_object(struct.new) do |(value, i), result|
-              result[columns[i].to_sym] = value
-            end.to_h)
+      def each
+        response = self.response
+        if response.is_a?(Neo4j::Server::CypherResponse)
+          response.each_data_row do |row|
+            yield row
           end
-        end
-      end
-      # Coerces result objects from structs to hashes in results. 
-      def to_node_enumeration(cypher = '', session = Neo4j::Session.current)
-        Enumerator.new do |yielder|
-          @result_index = 0
-          to_struct_enumeration(cypher).each do |row|
-            @row_index = 0
-            yielder << row.each_pair.each_with_object(@struct.new) do |(column, value), result|
-              result[column] = map_row_value(value, session)
-              @row_index += 1
-            end.to_h
-            @result_index += 1
-          end
+        else
+          raise UnsupportedDriverError, 'Neo4j::Embedded is not supported (yet)'
         end
       end
     end
