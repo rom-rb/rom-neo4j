fb45aa89b2ea463971b034c32fbe09ef5298d396
maetl <me@maetl.net>
2015-05-13 21:29:11 +1000

    Use VCR fixtures instead of connecting to a live Neo4j database

--- spec/integration/adapter_spec.rb
+++ spec/integration/adapter_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rom/lint/spec'
 
 describe 'Neo4j adapter' do
-  let(:setup) { ROM.setup(:neo4j, server_url, basic_auth: basic_auth_hash) }
+  let(:setup) { ROM.setup(:neo4j, neo4j_server_url) }
 
   context 'with movies relation and mapper' do
     let(:rom) do
@@ -39,17 +39,21 @@ describe 'Neo4j adapter' do
       rom.relation(:movies)
     end
 
-    it 'maps movies #by_title' do
-      movies.by_title('The Matrix').as(:movies).first.tap do |movie|
-        expect(movie).to be_a(Movie)
-        expect(movie.title).to eql('The Matrix')
-        expect(movie.released).to eql(1999)
-        expect(movie.tagline).to eql('Welcome to the Real World')
+    it 'maps movies #by_title' do |example|
+      VCR.use_cassette(example.description) do
+        movies.by_title('The Matrix').as(:movies).first.tap do |movie|
+          expect(movie).to be_a(Movie)
+          expect(movie.title).to eql('The Matrix')
+          expect(movie.released).to eql(1999)
+          expect(movie.tagline).to eql('Welcome to the Real World')
+        end
       end
     end
 
-    it 'maps movies #by_year' do
-      expect(movies.by_year(1999).to_a.count).to eq(4)
+    it 'maps movies #by_year' do |example|
+      VCR.use_cassette(example.description) do
+        expect(movies.by_year(1999).to_a.count).to eq(4)
+      end
     end
   end
 
@@ -79,10 +83,12 @@ describe 'Neo4j adapter' do
       rom.relation(:directors)
     end
 
-    it 'maps directors #by_movie_title' do
-      directors.by_movie('RescueDawn').as(:directors).first.tap do |director|
-        expect(director).to be_a(Director)
-        expect(director.name).to eql('Werner Herzog')
+    it 'maps directors #by_movie_title' do |example|
+      VCR.use_cassette(example.description) do
+        directors.by_movie('RescueDawn').as(:directors).first.tap do |director|
+          expect(director).to be_a(Director)
+          expect(director.name).to eql('Werner Herzog')
+        end
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,12 @@
 require 'rom-neo4j'
+require 'webmock'
+require 'vcr'
 
-def server_username
-  ENV['NEO4J_USERNAME'] || 'neo4j'
-end
-
-def server_password
-  ENV['NEO4J_PASSWORD'] || 'neo4jrb rules, ok?'
-end
-
-def basic_auth_hash
-  {
-    username: server_username,
-    password: server_password
-  }
-end
-
-def server_url
+def neo4j_server_url
   ENV['NEO4J_URL'] || 'http://localhost:7474'
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/neo4j_server'
+  config.hook_into :webmock
 end
